d6a6d9f5034438ab030d93d12f94fce8df76a74f
everton-lourens <evertonlourens@gmail.com>
2024-07-20 18:27:32 -0300

    uninstall lib unnecessary

--- src/hooks/usePlayerBackground.tsx
+++ src/hooks/usePlayerBackground.tsx
@@ -1,10 +1,9 @@
+/*
 import React from 'react'
 import { getColors } from 'react-native-image-colors'
 
 export const usePlayerBackground = (url: string) => {
-
 	const [colors, setColors] = React.useState(null)
-
 
 		getColors(url, {
 			fallback: '#228B22',
@@ -16,3 +15,4 @@ export const usePlayerBackground = (url: string) => {
 
 	return colors
 }
+*/
